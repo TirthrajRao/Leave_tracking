f381d925d16940b620a06960420a1f263825e631
komalsakhiya <komalsakhiya21@gmail.com>
2019-11-04 17:52:34 +0530

    for pull

--- platforms/ios/www/main-es2015.js
+++ platforms/ios/www/main-es2015.js
@@ -804,7 +804,7 @@ AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "config", function() { return config; });
 const ip = 'https://leavetracking.raoinformationtechnology.com:9000/';
-// const ip = 'http://192.168.1.73:3000/';
+// const ip = 'http://192.168.1.88:3000/';
 const baseUrl = ip;
 // const baseMediaUrl = 'http://192.168.1.73/leaveTracking-master/uploads/';
 // const baseMediaUrl = ip + "uploads/";
